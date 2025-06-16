ed352fb12fc1bee6ec251d88e00cb79f984a4ac4
peynadol <peynadol@gmail.com>
2025-06-16 22:47:19 +0100

    stop upvote triggering redirect

--- src/components/upvote-button.tsx
+++ src/components/upvote-button.tsx
@@ -7,11 +7,18 @@ const UpvoteButton = ({ id }) => {
   const suggestions = useFeedbackStore((state) => state.suggestions);
   const onUpvote = useFeedbackStore((state) => state.upvote);
   const item = suggestions.find((item) => item.id === id);
+
+  // extracted in order to prevent button click activating the link
+  const handleUpvote = (e) => {
+    e.preventDefault();
+    onUpvote(id);
+  };
+
   return (
     <Button
       variant="upvote"
       className="items-center gap-0 py-7 w-[50px] cursor-pointer"
-      onClick={() => onUpvote(id)}
+      onClick={handleUpvote}
     >
       <ChevronUp className="size-4 text-blue" />
       <span className="text-sm font-semibold">{item?.upvotes}</span>
